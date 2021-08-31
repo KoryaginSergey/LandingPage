0b86d38965c4366d71406bf1534c8a17a20dab9a
Koryagin Sergey <>
2021-08-31 11:00:48 +0300

    added WelcomeToAppVC

---
+++ Landing Page/Views/WelcomeToApp/WelcomeToAppViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeToAppViewController.swift
+//  Landing Page
+//
+//  Created by Admin on 30.08.2021.
+//
+
+import UIKit
+
+class WelcomeToAppViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
